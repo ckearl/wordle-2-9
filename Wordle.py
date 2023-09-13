c2e50bbcd759b197ba5e67ee38e5e5f96ed47fb8
Dallascampbell <dallasbcampbell@gmail.com>
2023-09-13 12:28:05 -0600

    Milestone 1 and 2

--- Wordle.py
+++ Wordle.py
@@ -14,12 +14,35 @@ random_word = random.choice(FIVE_LETTER_WORDS)
 
 
 def wordle():
+    row = 0
+    col = 0
 
-    def enter_action(s):
-        gw.show_message("You have to implement this method.")
+    def enter_action(s):  
+        row = 0
+        col = 0        
+        word = ""    
+        for x in range(N_COLS):
+            word += gw.get_square_letter(row,col) 
+            col = col + 1
+        
+        #gw.show_message(word)
 
+        if word.lower() in FIVE_LETTER_WORDS:
+            gw.show_message("That's a good word")
+        else: 
+            gw.show_message("Not in word list")
+        
+
+    
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+
+
+    for x in random_word:
+        gw.set_square_letter(row,col,x)
+        col = col + 1
+
+    
 print(random_word)
 # Startup code
 
