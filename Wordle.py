5ffc20b2206d9b4b1ddaaa37a85b377db5e6fe39
colelevi <christensencole4@gmail.com>
2023-09-14 15:40:35 -0600

    letters

--- Wordle.py
+++ Wordle.py
@@ -15,11 +15,46 @@ random_word = random.choice(FIVE_LETTER_WORDS)
 
 def wordle():
 
-    def enter_action(s):
-        gw.show_message("You have to implement this method.")
+    def enter_action(s):  
+        row = gw.get_current_row()
+        col = 0        
+        word = ""    
+        for x in range(N_COLS):
+            word += gw.get_square_letter(row,col) 
+            col = col + 1
+        
+        #gw.show_message(word)
 
+        if word.lower() in FIVE_LETTER_WORDS:
+            gw.show_message("That's a good word")
+            square = 0
+            for x in range(N_COLS):
+                letter = gw.get_square_letter(row,square)
+                if letter.lower() == random_word[square]:
+                    gw.set_square_color(row,square,"#66BB66")
+                elif letter.lower() in random_word:
+                    gw.set_square_color(row,square, "#CCBB66")
+                else:
+                    gw.set_square_color(row,square, "#999999")
+                square = square+1
+            gw.set_current_row(row+1)
+        else: 
+            gw.show_message("Not in word list")
+            # for x in range(N_COLS):
+            #     gw.set_square_letter(row,col," ")
+            #     col = col + 1
+        
+    
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+
+    gw.set_current_row(0)
+
+    # for x in random_word:
+    #     gw.set_square_letter(row,col,x)
+    #     col = col + 1
+
+    
 print(random_word)
 # Startup code
 
