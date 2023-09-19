c3a04d5fa799da8dceeef8a408c8b6f5b5be9633
Dallascampbell <dallasbcampbell@gmail.com>
2023-09-19 12:14:56 -0600

    Remove unneeded yellows

--- Wordle.py
+++ Wordle.py
@@ -12,6 +12,15 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 random_word = random.choice(FIVE_LETTER_WORDS)
 
+letter_count = {}
+
+for letter in random_word:
+    if letter in letter_count:
+        letter_count[letter] += 1
+    else:
+        letter_count[letter] = 1
+
+print(letter_count)
 
 def wordle():
     row = 0
@@ -30,15 +39,38 @@ def wordle():
         if word.lower() in FIVE_LETTER_WORDS:
             gw.show_message("That's a good word")
             square = 0
+            color_count = {}
+
             for x in range(N_COLS):
                 letter = gw.get_square_letter(row,square)
                 if letter.lower() == random_word[square]:
                     gw.set_square_color(row,square,"#66BB66")
+                    color = "green"
                 elif letter.lower() in random_word:
                     gw.set_square_color(row,square, "#CCBB66")
+                    color = "yellow"
                 else:
                     gw.set_square_color(row,square, "#999999")
-                square = square+1
+                    color = "gray"
+                
+                color_count[square] = color
+
+                square = square+1      
+
+            print(color_count)
+            square = 0
+            for x in range(N_COLS):
+                letter = gw.get_square_letter(row,square).lower()
+                print(letter)
+                if letter in letter_count and color_count[square] == "green":
+                    col = 0
+                    for t in range(N_COLS):
+                        if gw.get_square_letter(row,col).lower() == letter and color_count[col] != "green":
+                            gw.set_square_color(row,col, "#999999")
+                        col = col+1
+
+                square = square+1 
+            
             gw.set_current_row(row+1)
         elif (word[-1]) == " ":
             gw.show_message("Not enough letters")
